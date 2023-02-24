84b2f68e1d727fc29082aa6844b2fb989efec63e
KomarK0X <tomek.jarosz@spoko.pl>
2023-02-24 16:01:23 +0100

    Ugly style to update and nav basically done

--- src/Homepage/HomePage.tsx
+++ src/Homepage/HomePage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Styles from "./HomePage.module.scss"
+import { useState } from "react";
 function Homepage() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={Styles.HomePage}>
       <header>
@@ -16,10 +19,33 @@ function Homepage() {
             <li><a href="#">Contact</a></li>
           </ul>
         </nav>
+        <img
+          className={Styles.Hamburger}
+          src="../../img/menu.png"
+          alt="menu"
+          onClick={() => setOpen(true)}
+          />
       </header>
       <main>
         <h1>LET’s BUILD YOUR WEBSIDE</h1>
       </main>
+      {open ? (<div className={Styles.MobileMenu}>
+        <div className={Styles.Empty}></div>
+        <div className={Styles.MobileNav}>
+          <img
+            src="/../../img/close.png"
+            alt="close"
+            onClick={() => setOpen(false)}
+            />
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+       
+      </div>) : null}
     </div>
   );
 }
